3b657539779700090b8422372e300fff1e3743a0
Dorr-hlc <1726660621@qq.com>
2021-09-14 22:08:14 +0800

    新建登陆分支，组件，配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login/login'
+import M from 'minimatch'
 
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
     routes: [{
-        path: '/',
+        path: '/login',
+        name: 'login',
+        component: Login
     }]
 })
